778225928700df6033e806bcf3ae6b06089d60fd
ITAgiuseppe <giuseppe.archeo@yahoo.it>
2023-05-05 14:29:04 +0200

    Modifiche generali Layout

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,16 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+var clients = document.getElementById('clients');
+var services = document.getElementById('services');
+
+clients.addEventListener('click', function() {
+  $(clients).toggleClass("active");
+  $(".parent:not(#clients)").toggleClass("invisible");
+}, false);
+
+services.addEventListener('click', function() {
+  $(services).toggleClass("active");
+  $(".parent:not(#services)").toggleClass("invisible");
+}, false);
